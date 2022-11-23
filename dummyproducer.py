3beb36ace206b9b4e3a76f03d5a7ed3a5aa4856c
Manjujohn3 <manjujohn301@gmail.com>
2022-11-23 20:04:37 +0530

    producer

---
+++ dummyproducer.py
@@ -0,0 +1,22 @@
+import time
+from kafka import KafkaProducer
+
+bootstrap_server = ["localhost:9092"]
+
+topic = "nestDigital"
+
+producer = KafkaProducer(bootstrap_servers=bootstrap_server)
+
+producer = KafkaProducer()
+
+data = "Hi,Hello"
+
+def sendata():
+    message = producer.send(topic,bytes(data,"utf-8"))
+    metadata = message.get()
+    print(metadata.topic)
+    print(metadata.partition)
+    time.sleep(5)
+
+while True:
+    sendata()
